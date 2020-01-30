4bcfe4190a2393fae46640d58788b4de045aec69
manchandajayant <maanjayant@gmail.com>
2020-01-30 16:46:33 +0100

    add data and elements for form submission

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,16 @@
 function submitComment() {
-  console.log("Hello");
+  const inputField = document.getElementById("name");
+  const textField = document.getElementById("msg");
+  const name = inputField.value;
+  const msg = textField.value;
+
+  const comment = document.createElement("section");
+  const h3 = document.createElement("h3");
+  const p = document.createElement("p");
+
+  h3.innerHTML = `${name} said:`;
+  p.innerHTML = msg;
+  comment.classList.add("comment");
+  comment.appendChild(h3);
+  comment.appendChild(p);
 }
