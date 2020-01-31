0cd5fe3493e6cbb288299da9c444021b3b6cd4d2
manchandajayant <maanjayant@gmail.com>
2020-01-31 14:31:10 +0100

    added through the data

--- index.js
+++ index.js
@@ -343,3 +343,25 @@ const data = {
     }
   ]
 };
+const paintings = data.artObjects;
+console.log(paintings);
+
+// for (let id = 0; id < array.length; id++) {
+//   const element = array[id];
+
+// }
+
+for (let i = 0; i < paintings.length; i++) {
+  const painting = paintings[i];
+  console.log(painting.webImage.url);
+  const gallery = document.getElementById("gallery");
+  console.log("gallery", gallery);
+
+  //const imgRef = document.createElement("href");
+  //console.log(imgRef);
+  const imgCreate = document.createElement("img");
+  console.log(imgCreate);
+
+  imgCreate.src = painting.webImage.url;
+  gallery.appendChild(imgCreate);
+}
